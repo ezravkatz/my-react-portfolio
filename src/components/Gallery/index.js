38ba07f138e12800d3e4f2a15e8195bf8929bb31
ezravkatz <ezravkatz@gmail.com>
2022-08-15 19:22:18 -0400

    add more photos

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import one from '../../assets/portfolio/one.jpg'
 import two from '../../assets/portfolio/two.jpg'
 import three from '../../assets/portfolio/three.jpg'
+import four from '../../assets/portfolio/four.jpg'
 
 class Main extends Component {
 
 state = {
   index: 0,
-  picList:[one, two, three]
+  photoList:[one, two, three, four]
 }
 
 onClickNext= () => {
-  if (this.state.index + 1 === this.state.picList.length ){
+  if (this.state.index + 1 === this.state.photoList.length ){
       this.setState({ 
           index: 0 
         })
@@ -25,7 +26,7 @@ onClickNext= () => {
   onClickPrevious= () => {
     if (this.state.index - 1 === -1 ){
         this.setState({ 
-            index: this.state.picList.length - 1
+            index: this.state.photoList.length - 1
           })
       } else {
           this.setState({
@@ -37,7 +38,7 @@ onClickNext= () => {
 render() {
 return (
   <div>
-    <img id='portfolioPics' src={this.state.picList[this.state.index]} /> <br/>
+    <img id='portfolioPics' src={this.state.photoList[this.state.index]} /> <br/>
     <button style={{"fontSize":"18px"}} onClick={this.onClickPrevious}> Previous </button>
     <button style={{"margin-left":"5px", "fontSize":"18px"}} onClick={this.onClickNext}> Next </button>
   </div>
